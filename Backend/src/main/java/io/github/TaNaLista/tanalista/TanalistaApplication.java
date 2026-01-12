2e0f3c8494323e61cf2b026860dd415c2bf32a11
Derick Bessa de Almeida <derickbessa12@gmail.com>
2026-01-12 16:50:53 -0300

    ss

---
+++ Backend/src/main/java/io/github/TaNaLista/tanalista/TanalistaApplication.java
@@ -0,0 +1,13 @@
+package io.github.TaNaLista.tanalista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanalistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanalistaApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/io/github/TaNaLista/tanalista/TanalistaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.TaNaLista.tanalista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanalistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
